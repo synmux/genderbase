f1028c96f14e7d63267bdfffbbd2c5325ccee60e
Dave Williams <dave@dave.io>
2025-04-06 16:28:06 +0100

    trunk fix

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require_relative "boot"
 
-require "rails/all"
+require "rails/all" # Rails 8.0
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
