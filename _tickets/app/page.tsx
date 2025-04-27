5543630d49492aa6d70cd745a6c90957fd26c756
Dave Williams <dave@dave.io>
2025-04-27 12:22:20 +0100

    chore(src): Remove _tickets

--- _tickets/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { TicketExplorer } from "@/components/ticket-explorer"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-synthwave-bg">
-      <TicketExplorer csvUrl="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/linear-JmSWVvtcB3MlJMEY3ql5FO3p3iP8cV.csv" />
-    </main>
-  )
-}
