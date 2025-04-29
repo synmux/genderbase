4959af6b93d082f9e104738d631a358d9e6040a9
Dave Williams <dave@dave.io>
2025-04-29 16:41:23 +0100

    chore(ops): remove _tickets, move to its own git repo

--- _tickets/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { TicketExplorer } from "@/components/ticket-explorer"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-synthwave-bg">
-      <TicketExplorer csvUrl="/api/tickets" />
-    </main>
-  )
-}
